b5a08cd70e103f16cc2b45aba0a33733650ed3ca
sublimeSR <raut.samarth@gmail.com>
2018-10-13 00:14:11 +0530

    Hello 2

---
+++ Hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+cout << "Hello World !" << endl;
+cout << "Hello World 2 !" << endl;
+
+cout << "press ENTER to exit" << endl;
+cin.get();
+}
+
+
+
+
